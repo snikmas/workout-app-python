849eac480a379d920efee0b707d0e513fe6bb7cb
MarySnik <mary.snikk@gmail.com>
2025-12-27 23:55:12 +0800

    pref: some updates;

--- src/managers/database_manager.py
+++ src/managers/database_manager.py
@@ -14,16 +14,24 @@ class DatabaseManager:
         self.db = os.getenv("DB_NAME")
         self.port = os.getenv("DB_PORT")
         self.password = os.getenv("DB_PASSWORD")
+        self.cursor = None
 
 
     def get_connection(self):
-        db_con = psycopg2.connect(
-            host=self.host,
-            dbname=self.db,
-            user=self.user,
-            password=self.password,
-            port=self.port)
+        try:
+            db_con = psycopg2.connect(
+                host=self.host,
+                dbname=self.db,
+                user=self.user,
+                password=self.password,
+                port=self.port)
+            self.cursor = db_con.cursor() #use cursor to handle queries
+        except:
+            print("problem with conneciton")
         return db_con
+
+    def close_connection(self):
+        pass #here later have to close connecitno
 
     def is_user_exist(self, username, email):
         if self.db_con is None:
@@ -31,5 +39,11 @@ class DatabaseManager:
             if self.db_con is None:
                 print("Can't connect to the db. Back to the menu...")
                 return
+
+        #sql = "SELECT * FROM users WHERE nickname = ? OR email = ?"
+        #sql = "SELECT * FROM users WHERE (%s) OR (%s)"
+        result = self.cursor.execute("SELECT * FROM users WHERE nickname = (%s) OR email = (%s)", (username, email))
+        print(result)
+
 
         print("connected")
